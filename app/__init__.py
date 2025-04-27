8491dca6cab6da85e25466415dda0aa64981b2fb
Grace Muli <98580227+GraceMuli@users.noreply.github.com>
2025-04-27 13:49:44 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,9 @@ import os
 db = SQLAlchemy()
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, 
+             template_folder=os.path.join(os.path.pardir, 'templates'),
+             static_folder=os.path.join(os.path.pardir, 'static'))
     app.config['SECRET_KEY'] = 'your_secret_key'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../instance/app.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
